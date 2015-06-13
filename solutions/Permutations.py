d81602a5c8b2599d3a66b5cce44dec5429866741
Peter Liang <peter@userdeMacBook-Pro.local>
2015-06-13 23:28:44 +0800

    Finished kata 'Permutations'.

---
+++ solutions/Permutations.py
@@ -0,0 +1,27 @@
+"""
+http://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/python
+
+
+In this kata you have to create all permutations of an input string and remove duplicates, if present.
+This means, you have to shuffle all letters from the input in all possible orders.
+
+Examples:
+
+permutations('a'); # ['a']
+permutations('ab'); # ['ab', 'ba']
+permutations('aabb'); # ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+
+"""
+
+
+def permutations(string):
+    result = set([string])
+    if len(string) == 2:
+        result.add(string[1] + string[0])
+
+    elif len(string) > 2:
+        for i, c in enumerate(string):
+            for s in permutations(string[:i] + string[i + 1:]):
+                result.add(c + s)
+
+    return list(result)
