a1a31e7aad501abe285367bde731572d5427120e
Peter Liang <peter_liang@outlook.com>
2015-07-19 16:12:34 +0800

    Solved the kata 'Vigenère Autokey Cipher Helper'. http://www.codewars.com/kata/52d2e2be94d26fc622000735/solutions/python

--- solutions/Vigenere_Autokey_Cipher_Helper.py
+++ solutions/Vigenere_Autokey_Cipher_Helper.py
@@ -1,8 +1,14 @@
+# coding=utf-8
+"""
+Vigenère Autokey Cipher Helper
+http://www.codewars.com/kata/52d2e2be94d26fc622000735/train/python
+"""
+
+
 class VigenereAutokeyCipher:
     def __init__(self, key, abc):
         self.key = key
         self.abc = abc
-        self.start = ord(min(abc))
 
     def encode(self, text):
         result = []
@@ -10,8 +16,8 @@ class VigenereAutokeyCipher:
         index = 0
         for c in text:
             if c in self.abc:
-                offset = ord(key[index]) - self.start
-                result.append(chr(((ord(c) - self.start + offset) % len(self.abc)) + self.start))
+                offset = self.abc.index(key[index])
+                result.append(self.abc[(self.abc.index(c) + offset) % len(self.abc)])
                 index += 1
             else:
                 result.append(c)
@@ -23,8 +29,8 @@ class VigenereAutokeyCipher:
         index = 0
         for c in text:
             if c in self.abc:
-                offset = ord(key[index]) - self.start
-                decoded = chr(((ord(c) - self.start - offset) % len(self.abc)) + self.start)
+                offset = self.abc.index(key[index])
+                decoded = self.abc[(self.abc.index(c) - offset) % len(self.abc)]
                 result.append(decoded)
                 key += decoded
                 index += 1
