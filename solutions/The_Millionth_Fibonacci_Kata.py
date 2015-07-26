06aa3a1fa77c58e1405afec69ae650a97d199a92
Peter Liang <peter_liang@outlook.com>
2015-07-26 17:25:32 +0800

    Cached version.

--- solutions/The_Millionth_Fibonacci_Kata.py
+++ solutions/The_Millionth_Fibonacci_Kata.py
@@ -3,19 +3,20 @@ The Millionth Fibonacci Kata
 http://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/python
 """
 
+cache = {0: 0, 1: 1}
+
 
 def fib(n):
     """Calculates the nth Fibonacci number"""
-    if n == 1 or n == 0:
-        return n
-    a, b = 0, 1
-    if n > 1:
-        while n > 2:
-            a, b = b, a + b
-            n -= 1
-        return a + b
-    else:
-        while n < 0:
-            a, b = b - a, a
-            n += 1
-        return a
+    if n not in cache:
+        if n > 0:
+            maxkey, _ = max(cache.items())
+            while maxkey < n:
+                cache[maxkey + 1] = cache[maxkey - 1] + cache[maxkey]
+                maxkey += 1
+        else:
+            minkey, _ = min(cache.items())
+            while minkey > n:
+                cache[minkey - 1] = cache[minkey + 1] - cache[minkey]
+                minkey -= 1
+    return cache[n]
