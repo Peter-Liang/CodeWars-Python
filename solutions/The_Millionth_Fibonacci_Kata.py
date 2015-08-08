4d7bc923d953e50a72ca96aa3ff4682e9cdcc5c6
Peter Liang <peter_liang@outlook.com>
2015-08-08 21:40:39 +0800

    Solved the kata 'The Millionth Fibonacci Kata'. http://www.codewars.com/kata/53d40c1e2f13e331fc000c26/solutions/python

--- solutions/The_Millionth_Fibonacci_Kata.py
+++ solutions/The_Millionth_Fibonacci_Kata.py
@@ -3,20 +3,22 @@ The Millionth Fibonacci Kata
 http://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/python
 """
 
-cache = {0: 0, 1: 1}
-
 
 def fib(n):
     """Calculates the nth Fibonacci number"""
-    if n not in cache:
-        if n > 0:
-            maxkey, _ = max(cache.items())
-            while maxkey < n:
-                cache[maxkey + 1] = cache[maxkey - 1] + cache[maxkey]
-                maxkey += 1
-        else:
-            minkey, _ = min(cache.items())
-            while minkey > n:
-                cache[minkey - 1] = cache[minkey + 1] - cache[minkey]
-                minkey -= 1
-    return cache[n]
+    if n >= 0:
+        return fibiter(1, 0, 0, 1, n)
+    if n < 0:
+        a, b = 0, 1
+        for _ in xrange(0, n, -1):
+            a, b = b - a, a
+        return a
+
+
+def fibiter(a, b, p, q, count):
+    if count == 0:
+        return b
+    if count % 2 == 0:
+        return fibiter(a, b, p * p + q * q, q * q + 2 * p * q, count / 2)
+    else:
+        return fibiter(b * q + a * q + a * p, b * p + a * q, p, q, count - 1)
