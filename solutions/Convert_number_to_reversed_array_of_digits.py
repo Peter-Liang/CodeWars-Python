d268a2356aaf96c3f8c5dfe98fc98a11bac7ffdf
yzliang <yzliang@ctrip.com>
2017-09-30 17:30:04 +0800

    Solved 8 kyu kata 'Convert number to reversed array of digits'. http://www.codewars.com/kata/5583090cbe83f4fd8c000051/solutions/python

---
+++ solutions/Convert_number_to_reversed_array_of_digits.py
@@ -0,0 +1,22 @@
+"""
+8 kyu: Convert number to reversed array of digits
+
+Convert number to reversed array of digits
+
+Given a random number:
+
+C#: long;
+C++: unsigned long;
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+
+348597 => [7,9,5,8,4,3]
+"""
+
+
+def digitize(n):
+    return [int(s) for s in str(n)][::-1]
+
+
+print(digitize(35231))
